8e4b6023dfbb1d54afc8147eda325d475530a723
Mike Savage <mike.savage@gmail.com>
2018-02-07 04:05:40 -0500

    Update deployOpenShift.sh
    Call oc on remote master-000

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -652,7 +652,8 @@ then
 	echo $(date) "- Sleep for 20"
 	
 	sleep 20	
-	runuser -l $SUDOUSER -c  "oc label nodes --all logging-infra-fluentd=true logging=true"
+	#runuser -l $SUDOUSER -c  "oc label nodes --all logging-infra-fluentd=true logging=true"
+	runuser -l $SUDOUSER  -c  "ansible $MASTER-000 -a 'oc label nodes --all logging-infra-fluentd=true logging=true'"
 
 	runuser -l $SUDOUSER -c  "ansible all -b  -m service -a 'name=openvswitch state=restarted' "
 
